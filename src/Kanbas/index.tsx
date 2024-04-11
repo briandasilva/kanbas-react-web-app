767e0a2fe1c3f491df26f7480f8b2d98a5e6867f
Brian da Silva <dasilva.bri@northeastern.edu>
2024-04-11 17:46:45 -0400

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import axios from "axios";
+import Account from "./Account";
 
 function Kanbas() {
   const API_BASE = process.env.REACT_APP_API_BASE;
@@ -58,7 +59,7 @@ function Kanbas() {
         <div style={{ flexGrow: 1 }}>
           <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Account/*" element={<Account />} />
             <Route
               path="Dashboard"
               element={
